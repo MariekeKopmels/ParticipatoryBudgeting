0b2770a7ef330f150d159e162d3f39a411204622
Lonneke Pulles <lonneke.pulles@gmail.com>
2020-11-16 13:08:10 +0100

    Fixed approval_voting.py.

--- voting/approval_voting.py
+++ voting/approval_voting.py
@@ -8,31 +8,16 @@ import xlrd
 
 def approval(input):
     threshold = 50
-    print(type(input))
-    approval = {}
-    for i in range(0,no_voters):
-        print(input.iloc[i,1])
-    return 5 #input.transform(lambda x: x >= threshold)
-    # Alternative implementation (returns list instead of pandas dataframe)
-    #
-    # output_list = no_projects*[no_voters*[0]]
-    #     print(output_list)
-    #     # output = pd.DataFrame()
-    #     # output = input.DataFrame(data, columns=['project' + str(i) for i in range(0, no_projects)])
-    #     for voter in range(0, no_voters):
-    #         threshold = 50
-    #             # random.randint(int(max_utility / 2), max_utility)
-    #         # print(threshold)
-    #         for project in range(0, no_projects):
-    #             result = 1 if input.iloc[voter][project] >= threshold else 0
-    #             output_list[project][voter] = result
-    #             print('Voter: ' + str(voter) + ' Project: ' + str(project) + ' Result: ' + str(result))
-    #     print(output_list)
+    booleans = input.transform(lambda x: x >= threshold)
+    aggregate = booleans.sum(axis=0).tolist()
+    list = [(aggregate[i], i) for i in range(len(aggregate))]
+    list.sort(key=(lambda x: x[0]), reverse=True)
+    return [x[1] for x in list]
 
 
 if __name__ == '__main__':
     path = path_utilities()
-    utilities = pd.read_excel(path)
+    utilities = pd.read_excel(path, index_col=0)
     approval_voting = approval(utilities)
     print(approval_voting)
 
